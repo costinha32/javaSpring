bd49739a2fa0bee0c2b1db3b75105d7314ca085f
caio_i_silva <caio_i_silva@estudante.sesisenai.org.br>
2024-10-21 19:34:51 -0300

    java spring aula 2110

--- src/main/java/br/com/senai/controller/PessoaController.java
+++ src/main/java/br/com/senai/controller/PessoaController.java
@@ -16,19 +16,39 @@ public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
-
     @GetMapping
     public List<String> listarNome(){
         List<String> nomes = new ArrayList<String>();
-        nomes.add("Johnatan");
-        nomes.add("Caio");
-        nomes.add("Barba roza");
-
+        nomes.add("Caio, ex bebado");
+        nomes.add("Tiago o motorista de fuga");
+        nomes.add("Tod");
+        nomes.add("Hercules maguari");
+        nomes.add("Gabriel.");
         return nomes;
     }
+
     @PostMapping
-    public Pessoa cadastrarPessoa(@RequestBody Pessoa pessoa) {
+    public Pessoa cadastrarPessoa (@RequestBody Pessoa pessoa) {
+
         return pessoaService.cadastrarPessoa(pessoa);
     }
+    @GetMapping("/listar")
+    public List<Pessoa> listarPessoas() {
+        return pessoaService.listaPessoa();
+    }
 
+    @GetMapping("/busca/{id}")
+    public Pessoa buscarPessoaPorID(@PathVariable Long id) {
+        return pessoaService.buscarPessoaPorID(id);
+    }
+
+    @GetMapping("/buscar/{nomePessoa}")
+    public List<Pessoa> buscarPessoaPorNome (@PathVariable String nomePessoa) {
+        return pessoaService.buscarPessoaPorNome(nomePessoa);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirPessoa (@PathVariable Long id) {
+        pessoaService.excluirPessoa(id);
+    }
 }
--- src/main/java/br/com/senai/service/PessoaService.java
+++ src/main/java/br/com/senai/service/PessoaService.java
@@ -5,6 +5,7 @@ import br.com.senai.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class PessoaService {
@@ -12,8 +13,23 @@ public class PessoaService {
     @Autowired
     PessoaRepository pessoaRepository;
 
-    public Pessoa cadastrarPessoa(Pessoa pessoa) {
+    public Pessoa cadastrarPessoa (Pessoa pessoa) {
         return pessoaRepository.save(pessoa);
     }
 
+    public List<Pessoa> listaPessoa() {
+        return pessoaRepository.findAll();
+    }
+
+    public Pessoa buscarPessoaPorID (Long id) {
+        Pessoa pessoa = pessoaRepository.findById(id).orElseThrow(() -> new RuntimeException("Cadastro não encontrado. ID inválido"));
+        return pessoa;
+    }
+
+    public List<Pessoa> buscarPessoaPorNome(String nomePessoa) {
+        return pessoaRepository.buscarPessoaPorNome(nomePessoa);
+    }
+    public void excluirPessoa (Long id) {
+        pessoaRepository.deleteById(id);
+    }
 }
